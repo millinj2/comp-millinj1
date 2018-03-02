2db94368fbb333140eb1bd4ef23d038fef4f1246
Jenna Millin <millinj2@allegheny.edu>
2018-03-02 10:14:44 -0500

    uploading new data

--- newsret/newsret.py
+++ newsret/newsret.py
@@ -3,6 +3,39 @@ import datetime
 import urllib.request
 import sqlite3
 
+def getArticleSummary(source):
+    article.parse()
+    article.html
+    article.nlp()
+
+    value1 = article.summary
+    return value1
+
+def getArticleDate(source):
+    article.parse()
+    article.html
+    article.nlp()
+    value2 = article.publish_date
+    return value2
+
+# def getArticleHeadline(source):
+#     article.parse()
+#     article.html
+#     article.nlp()
+#     value3 = article.headline
+#     return value3
+
+def getKeyWords(source):
+    article.parse()
+    article.html
+    article.nlp()
+    words = article.keywords
+    filename = "articlekeywords.txt"
+    #create the file
+    f = open(filename,"w")
+    #write the data
+    f.write(str(words))
+
 def openFile(file):
 # read a list, return a dic
     try: #is the file there??
@@ -27,22 +60,15 @@ def begin(inFile):
     source = open(inFile,"r").read() # load the whole file.
     source = openFile(inFile)
 
-    print("is this working?")
-
-    article.parse()
-    article.html
-    article.nlp()
-
-    summary = article.summary
+    article = getArticleSummary(source)
     filename = "articlesummary.txt"
     #create the file
     f = open(filename,"w")
     #write the data
-    f.write(str(summary))
-    dt = article.publish_date
-    art_hdl = article.headline
-    article = article.summary
-    stock = "Apple"
+    f.write(str(article))
+    dt = getArticleDate(source)
+    art_hdl = "Article Headline2"#getArticleHeadline(source)
+    company = "Apple"
     DBfileName = "../compDB.db"
 
     baggagehand(dt, company, art_hdl, article, DBfileName)
@@ -55,7 +81,6 @@ def baggagehand(dt, company, art_hdl, article, DBfileName):
     # print " StockID:" ,stockid
     # print " Value: ", stockValue
     # print " Date: ", dt
-
 
     sqlite_file = DBfileName # the database file.
 
@@ -74,6 +99,32 @@ def baggagehand(dt, company, art_hdl, article, DBfileName):
 
     conn.close()
 
+# def baggagehandModel(stock_ID, art_hdl, sentiscore, keywords, DBfileName):
+# # method to get a file, open content, and populate database
+#
+#     print (" Baggagehand() We are saving to this file: ", DBfileName)
+#     # print " Data: "
+#     # print " StockID:" ,stockid
+#     # print " Value: ", stockValue
+#     # print " Date: ", dt
+#
+#     sqlite_file = DBfileName # the database file.
+#
+#     conn = sqlite3.connect(sqlite_file) # load the database file, defined above
+#     c = conn.cursor()
+#
+#     c.execute("INSERT INTO news VALUES (?, ?, ?, ?)", (dt, company, art_hdl, article))
+#     conn.commit()
+#     #print the tables from database
+#     result = c.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+#     print(result)
+#     #print everything in Stocks
+#     result2 = c.execute("SELECT * FROM news").fetchall()
+#     print (result2)
+#     row = c.fetchall()
+#
+#     conn.close()
+
 
 import sys
 
@@ -85,25 +136,12 @@ if __name__ == '__main__':
 
     data = response.read()
     text = repr(data).encode('utf-8')
-    file_ = open('article.html', 'wb')
+    filename = "article.html"
+    file_ = open(filename, 'wb')
     file_.write(data)
     file_.close()
 
-    # words = article.keywords
-    # filename = "articlekeywords.txt"
-    # #create the file
-    # f = open(filename,"w")
-    # #write the data
-    # f.write(str(words))
-
-    if len(sys.argv) == 2:
-         begin(sys.argv[1]) #,sys.argv[3], sys.argv[4]),sys.argv[5])
+    if len(sys.argv) == 1:
+         begin(sys.argv[0]) #,sys.argv[3], sys.argv[4]),sys.argv[5])
     else:
          sys.exit(0)
-
-    # summary = article.summary
-    # filename = "articlesummary.txt"
-    # #create the file
-    # f = open(filename,"w")
-    # #write the data
-    # f.write(str(summary))
